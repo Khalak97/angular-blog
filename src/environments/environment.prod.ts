ec065ddcf3b06a1925cffc3f833860f1cc948b31
Khalak97 <58552311+Khalak97@users.noreply.github.com>
2019-12-13 12:53:54 +0200

    login page works

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
-export const environment = {
-  production: true
+import { Environment } from './interface';
+
+export const environment: Environment = {
+  production: true,
+  apiKey: 'AIzaSyD9U3FNh7J-gxn64hNRL_Ff_-5iN8QrWuI'
 };
